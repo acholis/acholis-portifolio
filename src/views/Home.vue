b702688b3428269acaa3711e8568f2782a7a9e51
cabanga <manucabanga@gmail.com>
2020-05-25 21:04:30 +0100

    This commit update all project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,319 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <!--================Header Area =================-->
+        <header class="main_header_area">
+            <div class="main_menu_area">
+                <div class="container">
+                    <nav class="navbar navbar-default">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#"><img src="assets/img/logotipo.png" alt="" style="width: 90px;"></a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li><a href="#feature">DIFERENCIAIS</a></li>
+                            <li><a href="#screenshot">PORTIFOLIO</a></li>
+                            <li><a href="#contact">Contactos</a></li>
+                            <li><a href="#"><i class="fa fa-search"></i></a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!--================End Header Area =================-->
+
+        <!--================Slider Area =================-->
+        <section class="main_slider_area">
+            <div id="main_slider" class="rev_slider" data-version="5.3.1.6">
+                <ul> 
+                    <li data-index="rs-2972" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="default"  data-thumb="img/home-slider/slider-1.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Web Show" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                        <!-- MAIN IMAGE -->
+                        <!-- LAYERS -->
+                        <div class="slider_text_box">
+                            <div class="tp-caption first_text" 
+                                data-x="['center','center','center','center']" 
+                                data-y="['middle','middle','middle','middle']" 
+                                data-hoffset="['0','0','0','0']" 
+                                data-voffset="['0','0','0','0']" 
+                                data-fontsize="['200','100','100','100','60']"
+                                data-lineheight="['100','100','100','100','70']"
+                                data-width="['none','none','none','none']"
+                                data-height="none"
+                                data-whitespace="['nowrap','nowrap','nowrap','normal']"
+                                data-type="text" 
+                                data-responsive_offset="on" 
+                                data-frames='[{"from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","speed":1500,"to":"o:1;","delay":1700,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"x:left(R);","ease":"Power3.easeIn"}]'
+                                data-textAlign="['center','center','center','center']"
+                                data-paddingtop="[0,0,0,0]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[10,10,10,10]"
+                                data-paddingleft="[0,0,0,0]">Inovação
+                            </div>
+                            
+                        </div>
+                    </li>
+                    <li data-index="rs-2973" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="default"  data-thumb="assets/img/home-slider/slider-1.jpg"  data-rotate="0"  data-saveperformance="off"  data-title="Web Show" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
+                        <!-- LAYERS -->
+                        <div class="slider_text_box">
+                            <div class="tp-caption first_text" 
+                            data-x="['center','center','center','center']" 
+                            data-y="['middle','middle','middle','middle']" 
+                            data-hoffset="['0','0','0','0']" 
+                            data-voffset="['0','0','0','0']" 
+                            data-fontsize="['200','100','100','100','60']"
+                            data-lineheight="['100','100','100','100','70']"
+                            data-width="['none','none','none','none']"
+                            data-height="none"
+                            data-whitespace="['nowrap','nowrap','nowrap','normal']"
+                            data-type="text" 
+                            data-responsive_offset="on" 
+                            data-frames='[{"from":"z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;","speed":1500,"to":"o:1;","delay":1700,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"x:left(R);","ease":"Power3.easeIn"}]'
+                            data-textAlign="['center','center','center','center']"
+                            data-paddingtop="[0,0,0,0]"
+                            data-paddingright="[0,0,0,0]"
+                            data-paddingbottom="[10,10,10,10]"
+                            data-paddingleft="[0,0,0,0]">tecnologia</div>
+                            
+                            <div class="tp-caption secand_text" 
+                                data-x="['center','center','center','center']" 
+                                data-y="['middle','middle','middle','middle']" 
+                                data-hoffset="['0','0','0','0']" 
+                                data-voffset="['150','100','100','100','70']" 
+                                data-fontsize="['28','28','28','28']"
+                                data-lineheight="['38','38','38','38']"
+                                data-width="none"
+                                data-height="none"
+                                data-whitespace="nowrap"
+                                data-type="text" 
+                                data-responsive_offset="on" 
+                                data-frames='[{"from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":2000,"to":"o:1;","delay":1750,"ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"}]'
+                                data-textAlign="['left','left','left','left']"
+                                data-paddingtop="[0,0,0,0]"
+                                data-paddingright="[0,0,0,0]"
+                                data-paddingbottom="[0,0,0,0]"
+                                data-paddingleft="[0,0,0,0]">
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <!--================End Slider Area =================-->
+        
+        <!--================App Feature Area =================-->
+        <section class="app_feature_area" id="feature">
+            <div class="container">
+                <div class="main_title">
+                    <h2>OFERTAS PODEROSAS</h2>
+                    <p>Os melhores Talentos em TI para o sucesso dos seus projetos</p>
+                </div>
+                <div class="row app_feature_inner">
+                    <div class="col-md-3">
+                        <div class="app_feature_item_inner">
+                            <div class="app_feature_item">
+                                <div class="round_icon">
+                                    <i class="fa fa-plane"></i>
+                                </div>
+                                <a href="#"><h4>Acelere!</h4></a>
+                                <p>Sua empresa está competitiva no mercado em seu modelo de negócio atual? Acholis vai além da Transformação Digital.</p>
+                            </div>
+                            <div class="app_feature_item">
+                               <div class="round_icon">
+                                    <i class="fa fa-code"></i>
+                                </div>
+                                <a href="#"><h4>Desenvolvimento de Software</h4></a>
+                                <p>Pioneira em <strong>Desenvolvimento de Software</strong>, trabalhando com tecnologias voltadas para aplicativos
+								Web e <em>Mobile</em>. A empresa mantém seu DNA visionário através de <strong>Desenvolvimento Ágil</strong>.
+								</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="app_round_image">
+                            <img class="round_c" src="assets/img/round-circle.png" alt="">
+                            <img class="app_mobile" src="assets/img/app-mobile.png" alt="">
+                        </div>
+                        <div class="app_feature_item app_middle">
+                           <div class="round_icon">
+                                <i class="fa fa-desktop"></i>
+                            </div>
+                            <a href="#"><h4>Sites e Portais Corporativos</h4></a>
+                            <p>Desenvolvemos <strong>Sites</strong> e <strong>Portais Corporativos</strong> do jeito que você desejar através de Plataforma
+							de Gestão de Conteúdo (CMS - <em>Content Management System</em>) .
+							</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="app_feature_item_inner left_feature">
+                            <div class="app_feature_item">
+                                <div class="round_icon">
+                                    <i class="fa fa-cloud-download"></i>
+                                </div>
+                                <a href="#"><h4>Serviços Gerenciados</h4></a>
+                                <p>Seja em sistemas implantados na <strong>Nuvem</strong> ou em servidores próprios do cliente, temos a melhor opção
+								em <strong>Suporte</strong> de infraestrutura para que sua empresa esteja sempre conectada e nunca pare!
+								</p>
+                            </div>
+                            <div class="app_feature_item">
+                               <div class="round_icon">
+                                    <i class="fa fa-bullhorn"></i>
+                                </div>
+                                <a href="#"><h4>Marketing Digital Ágil</h4></a>
+                                <p>Utilizamos o melhor do <strong>Marketing Digital Ágil</strong> a seu favor. Através de técnicas de <em>Growth Hacking</em>
+								e integração via <strong>APIs</strong> (<em>Application Programming Interfaces</em>), impulsionamos os sistemas desenvolvidos
+								para nossos clientes.
+								</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End App Feature Area =================-->
+        
+		<br>
+		<br>
+		<br>
+        
+        <!--================MacBook Mockup Area =================-->
+        <section class="choose_us_area" style="padding-top: 150px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="install_left_content">
+							<div class="left_title">
+								<h2>QSHOP</h2>
+							</div>
+							<div class="install_content">
+								<h5>Plataforma online de productos frescos</h5>
+								<p>
+									QSHOP é uma solução web de compras online voltada a frescos (carne, peixe e outros).
+									Tem como maior objectivo gerar emprego (não só em termos de entregas), como tambêm consumir produtos nacionais e do interior.
+								</p>
+								<br>
+								<br>
+								<a class="gradient_bg_btn" href="https://qshop.ao/" target="_blank"><i class="fa fa-eye" aria-hidden="true"></i>ver plataforma</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<br>
+						<br>
+						<div class="install_mockup_img">
+							<img src="assets/img/macbook-res.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+        </section>
+        <!--================End MacBook Mockup Area =================-->
+
+        <!--================Footer Area =================-->
+        <footer class="footer_area" id="contact">
+
+            <div class="footer_widget_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-2 col-xs-6">
+                            <aside class="f_widget home_widget">
+                                <div class="f_title">
+                                    <img src="assets/img/marca.png" alt="" style="width: 100px;">
+                                </div>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="#">Diferenciais</a></li>
+                                    <li><a href="#">Portifolio</a></li>
+                                    <li><a href="#">Contactos</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-md-2 col-xs-6">
+                            <aside class="f_widget home_widget">
+                                <div class="f_title">
+                                    <h3>Supporte</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">hello@acholis.com</a></li>
+                                    <li><a href="#">+244 937 731 992</a></li>
+                                    <li><a href="#">+244 913 747 220</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-md-8 col-xs-12">
+                            <aside class="f_widget contact_widget">
+                                <div class="f_title">
+                                    <h3>Vamos conversar?</h3>
+                                </div>
+                                <form class="row contact_widget_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+                                    <div class="col-md-6">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <input type="text" class="form-control" id="name" name="name" placeholder="Seu nome completo">
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Nº telemóvel">
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <input type="email" class="form-control" id="email" name="email" placeholder="E-Mail">
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <input type="text" class="form-control" id="subject" name="subject" placeholder="Assunto">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <textarea class="form-control" name="message" id="message" rows="1" placeholder="Messagem......."></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <button type="submit" value="submit your quote" class="btn submint_btn form-control">ENVIAR AGORA</button>
+                                     </div>
+                                </form>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_copy_right">
+                <div class="container">
+                    <div class="pull-left">
+                        <p class="copyright">
+							Copyright &copy;2020 All rights reserved
+                        </p>
+                    </div>
+                    <div class="pull-right">
+                        <ul>
+                            <li><a href="https://qshop.ao/" target="_blank">Qshop</a></li>
+                            <li><a href="#">Contactos</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--================End Footer Area =================-->
+        
+        
+        
+
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Home'
+    }
 </script>
